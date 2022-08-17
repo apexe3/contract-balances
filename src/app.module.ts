498978c8978feaba46882db9e7d29338da455f5f
isfand-apexe <isfand@apexe3.com>
2022-08-17 14:39:29 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,9 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configuration, RedisCacheModule } from './services';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { RedisModule } from '@nestjs-modules/ioredis';
-
 @Module({
-  imports: [
-    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
-    RedisCacheModule,
-    RedisModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        config: {
-          url: `redis://${configService.get('REDIS_HOST')}:${configService.get(
-            'REDIS_PORT',
-          )}`,
-        },
-      }),
-    }),
-  ],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
